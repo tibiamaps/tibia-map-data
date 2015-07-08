7ac190c081d1639f8648c5a00976fcc2f668e37c
Mathias Bynens <mathias@qiwi.be>
2015-07-08 20:14:58 +0200

    Clean up markers

--- release.sh
+++ release.sh
@@ -6,12 +6,12 @@ file="Automap-${id}-without-markers.zip";
 echo "Generating maps without markers…";
 tibia-maps --from-data=./data --output-dir=./Automap --no-markers;
 echo "Saving maps without markers as \`${file}\`…";
-zip -q -r "${file}" Automap --exclude *.git* .DS_Store;
+zip -q -FS -r "${file}" Automap --exclude */.git* */.DS_Store;
 
 file="Automap-${id}-with-markers.zip";
 echo "Generating maps with markers…";
 tibia-maps --from-data=./data --output-dir=./Automap;
 echo "Saving maps without markers as \`${file}\`…";
-zip -q -r "${file}" Automap --exclude *.git* .DS_Store;
+zip -q -FS -r "${file}" Automap --exclude */.git* */.DS_Store;
 
 echo "All done.";
