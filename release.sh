bda135dfdf6da245dfc892488f78a0d6455bd1d5
Mathias Bynens <mathias@qiwi.be>
2015-07-08 18:58:18 +0200

    Tweak release script

--- release.sh
+++ release.sh
@@ -2,8 +2,16 @@
 
 id="$(date +%Y-%m-%d)";
 
+file="Automap-${id}-without-markers.zip";
+echo "Generating maps without markers…";
 tibia-maps --from-data=./data --output-dir=./Automap --no-markers;
-zip -q -r "Automap-${id}-without-markers.zip" Automap --exclude *.git* .DS_Store;
+echo "Saving maps without markers as \`${file}\`…";
+zip -q -r "${file}" Automap --exclude *.git* .DS_Store;
 
+file="Automap-${id}-with-markers.zip";
+echo "Generating maps with markers…";
 tibia-maps --from-data=./data --output-dir=./Automap;
-zip -q -r "Automap-${id}-with-markers.zip" Automap --exclude *.git* .DS_Store;
+echo "Saving maps without markers as \`${file}\`…";
+zip -q -r "${file}" Automap --exclude *.git* .DS_Store;
+
+echo "All done.";
