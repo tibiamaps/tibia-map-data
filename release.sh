15d956740a67b006af0b7551366766022ce5ecbd
Mathias Bynens <mathias@qiwi.be>
2015-12-03 23:56:47 +0100

    Add `--with-markers-only` flag to release script

--- release.sh
+++ release.sh
@@ -2,11 +2,13 @@
 
 id="$(date +%Y-%m-%d)";
 
-file="Automap-${id}-without-markers.zip";
-echo "Generating maps without markers…";
-tibia-maps --from-data=./data --output-dir=./Automap --no-markers;
-echo "Saving maps without markers as \`${file}\`…";
-zip -q -FS -r "${file}" Automap --exclude */.git* */.DS_Store;
+if [ "${1}" != '--with-markers-only' ]; then
+	file="Automap-${id}-without-markers.zip";
+	echo "Generating maps without markers…";
+	tibia-maps --from-data=./data --output-dir=./Automap --no-markers;
+	echo "Saving maps without markers as \`${file}\`…";
+	zip -q -FS -r "${file}" Automap --exclude */.git* */.DS_Store;
+fi;
 
 file="Automap-${id}-with-markers.zip";
 echo "Generating maps with markers…";
