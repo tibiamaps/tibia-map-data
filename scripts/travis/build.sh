2bbcfd120a7dbd19b4a7978d68d5d5a964cfec1f
Mathias Bynens <mathias@qiwi.be>
2016-03-16 00:25:14 +0100

    Improve ZIP file structure
    The generated ZIP files should contain a folder named `Automap` containing the` *.map` files.

--- scripts/travis/build.sh
+++ scripts/travis/build.sh
@@ -16,31 +16,33 @@ cp -R data/*.json "${DIST_DIR}/";
 echo "Compressing PNG images…";
 imagemin data/*.png "${DIST_DIR}/";
 
+cd "${DIST_DIR}";
+
 # Generate `dist/Automap-without-markers.zip`.
 file="Automap-without-markers.zip";
 echo "Generating maps without markers…";
-tibia-maps --from-data=./data --output-dir="${DIST_DIR}/Automap" --no-markers;
+tibia-maps --from-data=../data --output-dir=Automap --no-markers;
 echo "Saving maps without markers as \`${DIST_DIR}/${file}\`…";
-zip -q -FS -r "${DIST_DIR}/${file}" "${DIST_DIR}/Automap" --exclude */.git* */.DS_Store;
+zip -q -FS -r "${file}" Automap --exclude */.git* */.DS_Store;
 # Preserve `dist/Automap-without-markers/*.map`.
-mv "${DIST_DIR}/Automap" "${DIST_DIR}/Automap-without-markers";
+mv Automap Automap-without-markers;
 
 # Generate `dist/Automap-with-markers.zip`.
 file="Automap-with-markers.zip";
 echo "Generating maps with markers…";
-tibia-maps --from-data=./data --output-dir="${DIST_DIR}/Automap";
+tibia-maps --from-data=../data --output-dir=Automap;
 echo "Saving maps without markers as \`${DIST_DIR}/${file}\`…";
-zip -q -FS -r "${DIST_DIR}/${file}" "${DIST_DIR}/Automap" --exclude */.git* */.DS_Store;
+zip -q -FS -r "${file}" Automap --exclude */.git* */.DS_Store;
 # Preserve `dist/Automap-with-markers/*.map`.
-mv "${DIST_DIR}/Automap" "${DIST_DIR}/Automap-with-markers";
+mv Automap Automap-with-markers;
 
 # Create truncated & compressed (`*.map.gz`) versions of each `*.map` file.
 # Since these are only useful for online map viewers, only the map data is
 # needed; the pathfinding data and marker data can be removed.
 echo "Generating truncated & compressed map files for online mapper…";
-mkdir -p "${DIST_DIR}/mapper";
-for map in "${DIST_DIR}"/Automap-without-markers/*.map; do
-	head -c 65536 "${map}" | gzip -c > "${DIST_DIR}/mapper/$(basename ${map}).gz";
+mkdir -p mapper;
+for map in Automap-without-markers/*.map; do
+	head -c 65536 "${map}" | gzip -c > "mapper/$(basename ${map}).gz";
 done;
 
 echo "All done.";
