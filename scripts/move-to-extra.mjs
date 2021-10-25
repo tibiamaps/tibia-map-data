016d157768d05feaef9b0b45ab6089ca33ea15eb
Mathias Bynens <mathias@qiwi.be>
2021-10-25 23:47:23 +0200

    Move some more fire bomb spots to the rapid-respawn pack

--- scripts/move-to-extra.mjs
+++ scripts/move-to-extra.mjs
@@ -10,7 +10,7 @@ import { sortMarkers } from 'tibia-maps/src/sort-markers.mjs';
 const SPECIAL_ID = 'rapid-respawn';
 
 const isSpecial = (marker) => {
-	return marker.description === `Terrified elephant (fire bomb spot)`;
+	return marker.description === `Fire bomb spot`;
 	// https://tibiamaps.io/map#33686,30971,9:1
 	const TOP_LEFT_COORDINATE = { x: 33686, y: 30971 };
 	// https://tibiamaps.io/map#33862,31136,9:1
