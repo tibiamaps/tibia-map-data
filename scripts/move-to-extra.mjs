480edd284893a7a6ca0b8cbfd63e95e17809f870
Mathias Bynens <mathias@qiwi.be>
2021-10-25 23:44:08 +0200

    Add terrified elephant spawn points
    These are the exact spawn points of terrified elephants.

--- scripts/move-to-extra.mjs
+++ scripts/move-to-extra.mjs
@@ -7,10 +7,10 @@ const __dirname = path.dirname(__filename);
 
 import { sortMarkers } from 'tibia-maps/src/sort-markers.mjs';
 
-const SPECIAL_ID = 'anniversary';
+const SPECIAL_ID = 'rapid-respawn';
 
 const isSpecial = (marker) => {
-	return marker.description === `Piñata dragon`;
+	return marker.description === `Terrified elephant (fire bomb spot)`;
 	// https://tibiamaps.io/map#33686,30971,9:1
 	const TOP_LEFT_COORDINATE = { x: 33686, y: 30971 };
 	// https://tibiamaps.io/map#33862,31136,9:1
